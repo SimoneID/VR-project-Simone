f4f57c66d8fa92e056362e784e504583c5b207b8
Simone Ooms <simone120913@gmail.com>
2022-10-20 16:50:28 +0200

    Added the possibility to vary the intensity of the vibration based on distance from object (avatar)

--- Assets/Scripts/AvatarPosition.cs
+++ Assets/Scripts/AvatarPosition.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class AvatarPosition : MonoBehaviour
 {
+    [SerializeField] Transform Origin;
+    [SerializeField] float ActionTime = 2f;
+    [SerializeField] float WaitTime = 0.1f;
+    float CurrentProgress = 0f;
+    bool IsBusy = true;
+    
     float x;
     float y;
     float z;
     Vector3 pos;
+
+    [SerializeField] HapticEffectSO HBEffect;
     void Start()
     {
         x = Random.Range(-4.5f, 4.5f);
@@ -15,5 +23,19 @@ public class AvatarPosition : MonoBehaviour
         z = -4.5f;
         pos = new Vector3(x, y, z);
         transform.position = pos;
+    }
+
+    void Update()
+    {
+        CurrentProgress += Time.deltaTime / (IsBusy ? ActionTime : WaitTime);
+
+        if (CurrentProgress >= 1f)
+        {
+            if (!IsBusy)
+                HapticManager.PlayEffect(HBEffect, Origin.transform.position);
+
+            IsBusy = !IsBusy;
+            CurrentProgress = 0f;
+        }        
     }
 }
