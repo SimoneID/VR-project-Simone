b2184a799bad569f52cdbed0ce44026afdbbb96d
Simone Ooms <simone120913@gmail.com>
2022-10-20 15:04:48 +0200

    Added the ability to save file (csv) on internal storage of the Quest 2

--- Assets/Scripts/SavePosition.cs
+++ Assets/Scripts/SavePosition.cs
@@ -10,13 +10,14 @@ public class SavePosition : MonoBehaviour
     public float xPos, yPos, zPos, xRot, yRot, zRot;
     public GameObject MainCamera;
     public Vector3 LoadPosition;
+    public StreamWriter file;
 
-    [SerializeField] float _interval = 1f;
+    [SerializeField] float _interval = 3f;
     float _time;
 
     void Start()
     {
-        _time = 0f;
+        _time = 0f;        
     }
 
     void Update()
@@ -39,17 +40,28 @@ public class SavePosition : MonoBehaviour
         zRot = MainCamera.transform.rotation.z;
 
         //string filePath = @"C:\Users\simone\Documents\testfile.csv"; //On the PC at CWI
-        string filePath = @"C:\Users\20173880\Documents\testfile.csv"; //On my laptop
-        if (!File.Exists(filePath))
-        {
-            // Create a file to write to.
-            //string createText = "Hello and Welcome" + Environment.NewLine;
-            File.WriteAllText(filePath, "xPos,yPos,zPos,xRot,yRot,zRot" + Environment.NewLine);
-        }
-        //var content = "testing";
-        Debug.Log("Values of position are" + xPos + "," + yPos + "," + zPos);
-        Debug.Log("Values of the rotation are" + xRot + "," + yRot + "," + zRot);
-        File.AppendAllText(filePath, xPos + "," + yPos + "," + zPos + "," + xRot + "," + yRot + "," + zRot + Environment.NewLine);
+        //string filePath = @"C:\Users\20173880\Documents\testfile.csv"; //On my laptop
 
+        //if (!File.Exists(filePath))
+        //{
+        //    // Create a file to write to.
+        //    //string createText = "Hello and Welcome" + Environment.NewLine;
+        //    File.WriteAllText(filePath, "xPos,yPos,zPos,xRot,yRot,zRot" + Environment.NewLine);
+        //}
+
+        //var content = "testing";
+        //Debug.Log("Values of position are" + xPos + "," + yPos + "," + zPos);
+        //Debug.Log("Values of the rotation are" + xRot + "," + yRot + "," + zRot);
+        //File.AppendAllText(filePath, xPos + "," + yPos + "," + zPos + "," + xRot + "," + yRot + "," + zRot + Environment.NewLine);
+        
+        string fname = System.DateTime.Now.ToString("HH-mm-ss") + ".csv";
+        //string fname = "Testfile" + ".csv";
+        string path = Path.Combine(Application.persistentDataPath, fname);
+        file = new StreamWriter(path, true);
+
+        //file.WriteLine("Test");
+        file.WriteLine(xPos + "," + yPos + "," + zPos + "," + xRot + "," + yRot + "," + zRot);
+        file.Close();
+        //file.WriteLine(xPos + "," + yPos + "," + zPos + "," + xRot + "," + yRot + "," + zRot);
     }
 }
